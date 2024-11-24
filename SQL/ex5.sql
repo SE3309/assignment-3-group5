98aa803f339697b7a1a14861f1881939f546e731
Tejanvesh Gangavarapu <tgangava@uwo.ca>
2024-11-24 12:10:18 -0500

    Select Statements (LEFT JOIN & UNION)

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -14,3 +14,20 @@ FROM Truck t, Driver d, Finance f
 WHERE t.driverID=d.driverID
 	AND t.truckID=f.truckID
 ORDER BY f.truckID,f.totalAmount; -- first orders by truckID and then orders by total amount
+
+-- 5.3: SELECT-FROM-WHERE (LEFT JOIN and GROUP BY)
+SELECT t.truckID, t.licencePlateNumber, d.driverID, d.driverName
+FROM Driver d LEFT JOIN Truck t ON t.driverID = d.driverID
+			  LEFT JOIN Route r ON r.truckID = t.truckID
+WHERE d.workingStatus = 'ACTIVE'
+GROUP BY d.driverID, t.truckID, t.licencePlateNumber, d.driverName;
+
+-- 5.4: SELECT-FROM-WHERE (UNION AND ORDER BY)
+(SELECT truckID AS ID, damageData, damageDescription, 'Truck' AS VehicleType 
+FROM TruckDamageReport
+WHERE damageDescription IS NOT NULL)
+UNION ALL
+(SELECT trailerID AS ID, damageData, damageDescription, 'Trailer' AS VehicleType  
+FROM TrailerDamageReport
+WHERE damageDescription IS NOT NULL
+ORDER BY ID, damageData);
